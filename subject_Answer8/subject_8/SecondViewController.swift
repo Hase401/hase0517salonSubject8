101c493f395c8714a4935cf42ed90fe619d11d98
Hase401 <83523126+Hase401@users.noreply.github.com>
2021-05-17 20:23:06 +0900

    Add files via upload

---
+++ subject_Answer8/subject_8/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  subject_8
+//
+//  Created by 長谷川孝太 on 2021/05/16.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet private weak var sliderValueLabel: UILabel!
+    @IBOutlet private weak var slider2: UISlider!
+    
+    private let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        slider2.value = appDelegate.value
+        sliderValueLabel.text = String(appDelegate.value)
+    }
+    
+    @IBAction private func changeSliderValue(_ sender: UISlider) {
+        appDelegate.value = sender.value
+        sliderValueLabel.text = String(appDelegate.value)
+    }
+}
